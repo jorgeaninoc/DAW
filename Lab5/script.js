3af2c74d0a40987fec2c91792766d85437b29112
unknown <a01172309@itesm.mx>
2018-01-25 14:20:18 -0600

    Subir ejercicios realizados de Lab 5 con preguntas contestadas.

--- Lab5/script.js
+++ Lab5/script.js
@@ -67,7 +67,54 @@ function revisionProductos(){
     sum1 = (manzanas*10) + (peras*30) + (platanos*20);
     sum2 = sum1 + (sum1*.15);
     alert("Tú total es de: \n" + sum1 + " más el 15% de IVA: " + sum2);
+}
 
+function validarContrasenaYEncriptar(){
+    var clave1 = document.forms["clave"]["clave1"].value;
+    var clave2 = document.forms["clave"]["clave2"].value;
+    if(clave1.length == 0){
+        alert("No has insertado una contraseña.");
+        return 0;
+    }else if(clave2 !== clave1){
+        alert("La segunda contraseña no es igual a la primera.");
+        return 0;
+    }
+    mayus = 0;
+    num = 0;
+    if(clave1.length >= 6){
+        for(var i=0;i<clave1.length;i++){
+            if('0123456789'.indexOf(clave1.charAt(i)) !== -1){
+                num+=1;
+            }else if(clave1.charAt(i) == clave1.charAt(i).toUpperCase()){
+                mayus+=1;
+            }
+        }
+        if(mayus < 1){
+            alert("La contraseña debe tener al menos una mayuscula.");
+            return 0;
+        }
+        if(num < 1){
+            alert("La contraseña debe tener al menos un número.");
+            return 0;
+        }
+        document.forms["clave"]["clave3"].value = cesar(clave1,3);
+    }else{
+        alert("La contraseña debe tener 6 o más caracteres.");
+        return 0;
+    }
+}
 
-
+function cesar(plaintext, shiftAmount) {
+    var ciphertext = "";
+    for(var i = 0; i < plaintext.length; i++) {
+        var plainCharacter = plaintext.charCodeAt(i);
+        if(plainCharacter >= 97 && plainCharacter <= 122) {
+            ciphertext += String.fromCharCode((plainCharacter - 97 + shiftAmount) % 26 + 97);
+        } else if(plainCharacter >= 65 && plainCharacter <= 90) {
+            ciphertext += String.fromCharCode((plainCharacter - 65 + shiftAmount) % 26 + 65);
+        } else {
+            ciphertext += String.fromCharCode(plainCharacter);
+        }
+    }
+    return ciphertext;
 }
